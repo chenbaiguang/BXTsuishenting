c36eeccf5effcde0802232dcfb0c1d655e14a0eb
cbg <cbg@cbgdemac-mini.local>
2016-11-29 18:03:33 +0800

    原来写的全部没了，重新写一份。初始化项目。

---
+++ BXTsuishenting/BXTsuishenting/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BXTsuishenting
+//
+//  Created by cbg on 16/11/29.
+//  Copyright © 2016年 cbg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
